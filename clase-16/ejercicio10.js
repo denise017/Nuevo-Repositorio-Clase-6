f0bfc3a055dc24d70425898f19ffbff3e759b0db
Denise Maldonado <celeste41956@gmail.com>
2026-02-02 03:17:18 -0300

    Delete clase-16 directory

--- clase-16/ejercicio10.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-// ejercicio 10 construir una frase a partir de palabras
-
-let palabras = ['Me', 'gusta', 'aprender', 'JavaScript'];
-
-// join une todo en un solo string con espacios
-let frase = palabras.join(' ');
-
-console.log(frase);
--- clase-16/ejercicio5.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-// ejercicio 5 recorrer una lista e imprimir cada elemento
-
-let frutas = ['manzana', 'banana', 'cereza'];
-
-// foreach solo recorre no devuelve nada
-frutas.forEach(function(fruta){
-  console.log(fruta);
-});
